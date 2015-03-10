da458753c8777746e5aa4bbdc82c079e06688bb5
Amortized <rmanghwani@ebay.com>
2015-03-09 20:59:20 -0700

    adding lcs

--- sample.cpp
+++ sample.cpp
@@ -77,6 +77,88 @@ void stringPermuations(string x, int swapIndex, int len)
    }
 }
 
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+void longestCommonSubsequence(string s1, string s2)
+{
+   int LCS[s1.length()+1][s2.length()+1];
+   int backTrack[s1.length()+1][s2.length()+1]; //3 - indicates match; 1 - indicates left; 2 - indicates upwards
+
+   int i,j;
+   for(i = 0; i <= s1.length(); i++)
+   {
+    for(j = 0 ; j <= s2.length(); j++)
+    {
+      if (i == 0 or j == 0)
+      {
+        LCS[i][j] = 0;
+        if(i == 0 and j == 0)
+        {
+          backTrack[i][j] = -1;
+        }
+        else if (i == 0) {
+          backTrack[i][j] = 1;
+        }
+        else
+        {
+          backTrack[i][j] = 2; 
+        }
+      }
+      else if(s1[i-1] == s2[j-1])
+      {
+        LCS[i][j] = LCS[i-1][j-1] + 1;
+        backTrack[i][j] = 3;
+      }
+      else
+      {
+        if(LCS[i][j-1] >= LCS[i-1][j])
+        {
+          LCS[i][j]       =  LCS[i][j-1];
+          backTrack[i][j] =  1;
+        }
+        else
+        {
+          LCS[i][j]       =  LCS[i-1][j];
+          backTrack[i][j] =  2;
+        }
+      }    
+    }    
+   }
+
+
+   i = s1.length();
+   j = s2.length();
+
+   cout << "LCS : ";
+   while(i > 0 or j > 0)
+   {
+     if(backTrack[i][j] == 3)
+     {
+       cout << s1[i-1] << " ";
+       i--;
+       j--;
+     } 
+     else if (backTrack[i][j] == 2)
+     {
+       if (i > 0)
+       {
+        i--;
+       }
+     }
+     else 
+     {
+       if (j > 0)
+       {
+        j--;
+       }
+     }
+   }
+  
+
+}
 
 
 int main()
@@ -86,7 +168,8 @@ int main()
     findNoAppearingOnce(v);
 
     string s ("ABC");
-    stringPermuations(s, 0, s.length());
+    //stringPermuations(s, 0, s.length());
+    longestCommonSubsequence("AGGTAB", "GXTXAYB");
 
 	return 0;
 }
